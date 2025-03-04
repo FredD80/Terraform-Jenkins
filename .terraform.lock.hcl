e83963a9d8d4e90f6608d7d0664873aa8ea06cfc
Fred Dixon <mrdixon1213@gmail.com>
2025-03-04 09:57:09 -0500

    initial commit

---
+++ .terraform.lock.hcl
@@ -0,0 +1,63 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.89.0"
+  constraints = ">= 3.29.0, >= 4.66.0, >= 5.79.0"
+  hashes = [
+    "h1:rFvk42jJEKiSUhK1cbERfNgYm4mD+8tq0ZcxCwpXSJs=",
+    "zh:0e55784d6effc33b9098ffab7fb77a242e0223a59cdcf964caa0be94d14684af",
+    "zh:23c64f3eaeffcafb007c89db3dfca94c8adf06b120af55abddaca55a6c6c924c",
+    "zh:338f620133cb607ce980f1725a0a78f61cbd42f4c601808ec1ee01a6c16c9811",
+    "zh:6ab0499172f17484d7b39924cf06782789df1473d31ebae0c7f3294f6e7a1227",
+    "zh:6dcde3e29e538cdf80971cbdce3b285056fd0e31dd64b02d2dcdf4c02f21d0a9",
+    "zh:75c9b594d77c9125bfb1aaf3fbd77a49e392841d53029b5726eb71d64de1233e",
+    "zh:7b334c23091e7b4c142e378416586292197c40a31a5bdb3b29c4f9afddd286f0",
+    "zh:991bbba72e5eb6eb351f466d68080992f5b0495f862a6723f386d1b4c965aa7d",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:9bd2f12eef4a5dceafc211ab3b9a63f0e3e224007a60c1bbb842f76e0377033d",
+    "zh:b1ac1eb3b3e1a79fa5e5ad3364615f23b9ee0b093ceeb809fd386a4d40e7abb4",
+    "zh:cea91f43151b30c428c441b97c3b98bf1e5fb72ef72f6971308e3895e23437f4",
+    "zh:d3f000a1696a43d8186a516aace7d476d1fd76443627980504133477e19c8ecb",
+    "zh:d6f526fbbb3e51b3acc3b9640a158f7acc4a089632fca8ec6db430b450673f25",
+    "zh:e0c542950f96c93e761d50602e449fef8447f1389a6d5242a0a7dc9b06826d0b",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/http" {
+  version = "3.4.5"
+  hashes = [
+    "h1:eSVCYfvn5JyV3LC0+mrLlLtgLv4B+RWeNqz02miBcMY=",
+    "zh:2072006c177efc101471f3d5eb8e1d8e6c68778cbfd6db3d3f22f59cfe6ce6ae",
+    "zh:3ac4cc0efe11ee054300769cfcc37491433937a8824621d1f8f7a18e7401da87",
+    "zh:63997e5457c9ddf9cfff17bd7bf9f083cbeff3105452045662109dd6be499ef9",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:826819bb8ab7d6e3095f597083d5b1ab93d1854312b9e1b6c18288fff9664f34",
+    "zh:8ad74e7d8ec2e226a73d49c7c317108f61a4cb803972fb3f945d1709d5115fcd",
+    "zh:a609ca9e0c91d250ac80295e39d5f524e8c0872d33ba8fde3c3e41893b4b015d",
+    "zh:ae07d19babc452f63f6a6511b944990e819dc20687b6c8f01d1676812f5ada53",
+    "zh:b7c827dc32a1a5d77185a78cd391b01217894b384f58169f98a96d683730d8ce",
+    "zh:d045e3db9f5e39ce78860d3fd94e04604fcbe246f6fe346ee50a971f936e9ccd",
+    "zh:ec28f9b52c74edd47eebbb5c254a6df5706360cde5ccd65097976efca23a2977",
+    "zh:f24982eaa7d34fd66554c3cf94873713a0dff14da9ea4c4be0cc76f1a6146d59",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.5.2"
+  hashes = [
+    "h1:IyFbOIO6mhikFNL/2h1iZJ6kyN3U00jgkpCLUCThAfE=",
+    "zh:136299545178ce281c56f36965bf91c35407c11897f7082b3b983d86cb79b511",
+    "zh:3b4486858aa9cb8163378722b642c57c529b6c64bfbfc9461d940a84cd66ebea",
+    "zh:4855ee628ead847741aa4f4fc9bed50cfdbf197f2912775dd9fe7bc43fa077c0",
+    "zh:4b8cd2583d1edcac4011caafe8afb7a95e8110a607a1d5fb87d921178074a69b",
+    "zh:52084ddaff8c8cd3f9e7bcb7ce4dc1eab00602912c96da43c29b4762dc376038",
+    "zh:71562d330d3f92d79b2952ffdda0dad167e952e46200c767dd30c6af8d7c0ed3",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:805f81ade06ff68fa8b908d31892eaed5c180ae031c77ad35f82cb7a74b97cf4",
+    "zh:8b6b3ebeaaa8e38dd04e56996abe80db9be6f4c1df75ac3cccc77642899bd464",
+    "zh:ad07750576b99248037b897de71113cc19b1a8d0bc235eb99173cc83d0de3b1b",
+    "zh:b9f1c3bfadb74068f5c205292badb0661e17ac05eb23bfe8bd809691e4583d0e",
+    "zh:cc4cbcd67414fefb111c1bf7ab0bc4beb8c0b553d01719ad17de9a047adff4d1",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,173 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-project54321"
+    key    = "terraform-project54321/backend/pywebsite.tf"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "local" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "website-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.0.101.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    name        = "website-vpc"
+  }
+}
+
+data "aws_ami" "ubuntu_2404" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/*-24.04-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+}
+
+output "ami_id" {
+  value = data.aws_ami.ubuntu_2404.id
+}
+
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name                        = "website-Server"
+  instance_type               = "t3.micro"
+  ami                         = data.aws_ami.ubuntu_2404.id
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  iam_instance_profile        = "Managed-EC2"
+  key_name                    = "FD_CLI_Keypair"
+  vpc_security_group_ids      = [module.security_group.security_group_id]
+
+  root_block_device = [
+        
+        {
+    device_name           = "/dev/xvda" # Or /dev/sda1, etc. - Check your AMI documentation
+    volume_type           = "gp3"       # or gp2, io1, io2, st1, sc1
+    volume_size           = 32          # Size in GiB
+    encrypted             = true        # Recommended
+    delete_on_termination = true        # Recommended (so volume is deleted when instance is terminated)
+    
+      }
+  ]
+    user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update -y
+              sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+              sudo usermod -aG docker ubuntu
+              newgrp docker
+              sudo docker run -d --name myapp -p 80:5000 ${var.docker_image_tag}
+
+              EOF
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    name        = "website-server"
+  }
+}
+
+output "ec2_instance_public_ip" {
+  value = module.ec2_instance.public_ip
+}
+
+output "ec2_instance_private_ip" {
+  value = module.ec2_instance.private_ip
+}
+
+output "ec2_instance_public_dns" {
+  value = module.ec2_instance.public_dns
+}
+
+output "ec2_instance_instance_id" {
+  value = module.ec2_instance.id
+}
+
+module "security_group" {
+  source      = "terraform-aws-modules/security-group/aws"
+  name        = "front-sg"
+  description = "Security group for website server"
+  vpc_id      = module.vpc.vpc_id
+
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["all-icmp", "https-443-tcp", "http-80-tcp"]
+  ingress_with_cidr_blocks = [
+
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "${local.validated_ip}/32"
+    }
+
+ 
+  ] 
+
+  egress_rules = ["all-all"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    name        = "front-sg"
+  }
+
+}
+output "security_group_id" {
+  value = module.security_group.security_group_id
+}
+
+data "http" "my_ip" {
+  url = "https://checkip.amazonaws.com/"
+}
+
+locals {
+  # Remove whitespace and newlines from the response
+  current_ip = chomp(data.http.my_ip.response_body)
+
+  # Validate the IP format using regex
+  validated_ip = can(
+    regex(
+      "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+      local.current_ip
+    )
+  ) ? local.current_ip : null
+}
+
+variable "docker_image_tag" {
+  description = "Docker image tag to deploy"
+  default     = "latest"
+}
